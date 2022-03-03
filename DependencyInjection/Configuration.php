d5355bbe494d05567e0da7f790ae0a54c857eee0
Yves Maerschalck <yves.maerschalck@intracto.com>
2022-03-03 11:07:52 +0100

    Upgrade to symfony 5.4 - fix deprecations

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,9 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('intracto_lti');
-
+        $treeBuilder = new TreeBuilder('intracto_lti');
         return $treeBuilder;
     }
 }
